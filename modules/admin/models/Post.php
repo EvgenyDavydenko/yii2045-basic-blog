fdb212951ad81374941a1411cd83b98b22674f86
EvgenyDavydenko <frontend82@mail.ru>
2023-01-31 23:16:05 +0300

    generate CRUD for Post

---
+++ modules/admin/models/Post.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property string $title
+ * @property string $excerpt
+ * @property string $text
+ * @property string|null $keywords
+ * @property string|null $description
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'title', 'excerpt', 'text'], 'required'],
+            [['category_id'], 'integer'],
+            [['text'], 'string'],
+            [['title', 'excerpt', 'keywords', 'description'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_id' => 'Category ID',
+            'title' => 'Title',
+            'excerpt' => 'Excerpt',
+            'text' => 'Text',
+            'keywords' => 'Keywords',
+            'description' => 'Description',
+        ];
+    }
+}
---
+++ modules/admin/views/post/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\modules\admin\models\Post $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'excerpt')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'keywords')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
